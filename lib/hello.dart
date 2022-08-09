71a70474706687e1bee081c8286ef8e1cba68a47
Thongthong Junkla <junkla_t@silpakorn.edu>
2022-08-09 16:18:07 +0700

    Initial commit

--- lib/hello.dart
+++ lib/hello.dart
@@ -15,26 +15,25 @@ void main() {
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!); // ! = non-null
 
-    if (guess == null){
+    if (guess == null) {
       print('║ Please enter "Number" only');
       print('╟──────────────────────────────────────────────────');
       continue;
     }
 
-      result = game.doGuess(guess);
-      count++;
+    result = game.doGuess(guess);
+    count++;
 
-      if (result == 1) {
-        print('║ $guess is too High ▲');
-      } else if (result == 2) {
-        print('║ $guess is too Low ▼');
-      }else if (result == 3) {
-        print('║ $guess is Correct ♥, TOTAL GUESSES : $count');
-      }
+    if (result == 1) {
+      print('║ $guess is too High ▲');
+    } else if (result == 2) {
+      print('║ $guess is too Low ▼');
+    } else if (result == 3) {
+      print('║ $guess is Correct ♥, TOTAL GUESSES : $count');
+    }
 
     print('╟──────────────────────────────────────────────────');
-
-  } while(result != 3);
+  } while (result != 3);
 
   print('║                      THE END');
   print("╚══════════════════════════════════════════════════");
